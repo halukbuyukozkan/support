0a2b7ecb184d16ce98b07067bf610e29585e0ad8
Tunahan Caliskan <mail@tunahancaliskan.com>
2021-12-04 14:27:25 +0300

    Jetstrap installed.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use NascentAfrica\Jetstrap\JetstrapFacade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
+        if (App::environment('local')) {
+            JetstrapFacade::useAdminLte3();
+        }
     }
 }
