34309febf45bd1ba63ab640e5412860dd9aa43bf
tuzlu07x <fatihtuzlu07@icloud.com>
2022-07-06 12:39:10 +0300

    delete component fixed.

--- resources/views/components/delete-card.blade.php
+++ resources/views/components/delete-card.blade.php
@@ -1,18 +1,24 @@
-@props(['active', 'icon'])
+@props(['active', 'route'])
 
 @php
 $classes = $active ?? false ? 'delete-link active font-weight-bolder' : 'delete-link';
 @endphp
 
-<div class="card-body text-danger">
-    <p>{{ __('Do you want to delete this company?') }}</p>
-    <form action="{{ $slot }}" method="POST" class="d-inline-block"
-        onsubmit="return confirm('{{ __('Are you sure?') }}');">
-        @csrf
-        @method('DELETE')
-        <button class="btn btn-block btn-danger">
-            <i class="fas fa-trash"></i>
-            {{ __('Delete') }}
-        </button>
-    </form>
+<div class="card card-danger">
+    <div class="card-header">
+        <i class="fas fa-times"></i>
+        {{ __('Deleting') }}
+    </div>
+    <div class="card-body text-danger">
+        <p>{{ $slot }}</p>
+        <form action="{{ $route }}"method="POST" class="d-inline-block"
+            onsubmit="return confirm('{{ __('Are you sure?') }}');">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-block btn-danger">
+                <i class="fas fa-trash"></i>
+                {{ __('Delete') }}
+            </button>
+        </form>
+    </div>
 </div>
