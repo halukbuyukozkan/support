6fc40fcb8a55e7fb4731b5910baf5eb54015d029
halukbuyukozkan <halukbuyukozkan@gmail.com>
2022-08-15 09:01:40 +0300

    nav added to show page

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -5,6 +5,27 @@
         </h2>
     </x-slot>
 
+    <section>
+        <div class="row mb-4">
+            <div class="col">
+                <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a @if(request()->routeIs('admin.user.show*')) class="nav-link active" @else class="nav-link" @endif name="references" href="{{ route('admin.user.show', $user) }}"><i class="fas fa-user px-1"></i></i>Show</a>
+                    </li>
+                    <li class="nav-item">
+                        <a @if(request()->routeIs('admin.user.edit*')) class="nav-link active" @else class="nav-link" @endif name="references" href="{{ route('admin.user.edit', $user) }}"><i class="fas fa-edit px-1"></i>Edit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a @if(request()->routeIs('admin.user.ticket*')) class="nav-link active" @else class="nav-link" @endif name="references" href="{{ route('admin.user.ticket.index', $user) }}"><i class="fas fa-ticket-alt px-1"></i>Tickets</a>
+                    </li>
+                    <li class="nav-item">
+                        <a @if(request()->routeIs('admin.user.service*')) class="nav-link active" @else class="nav-link" @endif name="references" href="{{ route('admin.user.service.index', $user) }}"><i class="fas fa-bookmark px-1"></i>Services</a>
+                    </li>
+                </ul>
+            </div>
+        <div>
+    </section>
+
     <div class="card col-md-6">
         <div class="card-header">
           Active Tickets
